df308fdf73c2a7ae81651c68bde21e9e27b7c8f2
15427498 <alexandrecolmant14@gmail.com>
2020-02-11 15:24:10 +0100

    GIT Commit test.

--- InstallPackage.R
+++
@@ -1,9 +0,0 @@
-
-
-path <- "~/Bitbucket/james-bayer"      
-install.packages(file.path(path, "BCS.JAMES3_1.0.94.tar.gz"), 
-  lib="~/BCSJames3.0Installation", 
-  repos=NULL, 
-  INSTALL_opts = "--no-multiarch")
-
-
---
+++ IntysConsultingRProject/InstallPackage.R
@@ -0,0 +1,9 @@
+
+
+path <- "~"      
+install.packages(file.path(path, "IntysConsulting_1.0.0.tar.gz"), 
+  lib="~", 
+  repos=NULL, 
+  INSTALL_opts = "--no-multiarch")
+
+
