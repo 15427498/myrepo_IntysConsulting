9d195b654f033d0f20dff01743e0f5f1330780b6
15427498 <alexandrecolmant14@gmail.com>
2020-02-11 14:30:46 +0100

    First Commit. GIT.

---
+++ InstallPackage.R
@@ -0,0 +1,9 @@
+
+
+path <- "~/Bitbucket/james-bayer"      
+install.packages(file.path(path, "BCS.JAMES3_1.0.94.tar.gz"), 
+  lib="~/BCSJames3.0Installation", 
+  repos=NULL, 
+  INSTALL_opts = "--no-multiarch")
+
+
